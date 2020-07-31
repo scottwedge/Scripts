7828bffc0b27457e205bfb06c1b35c3c980170da
swedge <scott.wedge@gmail.com>
2020-07-30 23:14:17 -0400

    Can return values < 256 but above 256 only last 8 bits are returned apparently so my UID=1000 returns 232

---
+++ return-uid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# To help calling routine determine if the user is root or not
+# display the UID and then
+# return the value of UID
+
+function r()
+{
+    echo "UID is ${UID}"
+    j=${UID}
+    exit 251
+}
+
+
+r
