c77af8d459ae61402e7b92c0d3ab46197364130b
swedge <scott.wedge@gmail.com>
2020-08-03 20:28:42 -0400

    Variables in all capitals, with or without brackets

---
+++ simple_var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SKILL="shell script"
+
+echo "I want to be good at ${SKILL}ing"
+echo "I want to be good at $SKILLing"
+
+SKILL2="read"
+echo "I want to be good at ${SKILL2}ing"
+echo "I want to be good at $SKILL2ing"
+
