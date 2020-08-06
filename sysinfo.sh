bd8d62d03a9ec142dbf34d6c6164cb9ecb6790ad
swedge <scott.wedge@gmail.com>
2020-08-06 16:02:46 -0400

    Initial script with only pseudocode comments

---
+++ sysinfo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script displays information about the system on which it is executed.
+
+
+# Tell the user that the script is starting.
+
+
+# Display the hostname of the system.
+
+
+# Display the current date and time when this information was collected.
+
+# Display the kernel release followed by the architecture.
+
+# Display the disk usage in a human readable format.
+
+# End the script by letting the user know that it is done.
