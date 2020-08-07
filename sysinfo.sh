a30754d354a3b49cc93be762c74b267a063a0055
swedge <scott.wedge@gmail.com>
2020-08-07 18:01:12 -0400

    Add some code to the pseudocode

--- sysinfo.sh
+++ sysinfo.sh
@@ -4,14 +4,18 @@
 
 
 # Tell the user that the script is starting.
+echo "Script starting ..."
 
 
 # Display the hostname of the system.
+echo "Running on host $HOSTNAME"
 
 
 # Display the current date and time when this information was collected.
+echo "Date and time is: `date`"
 
 # Display the kernel release followed by the architecture.
+echo "Kernel release is: `uname -r` and architecture is: `uname -s`"
 
 # Display the disk usage in a human readable format.
 
