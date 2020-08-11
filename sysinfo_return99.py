2166ff6328fa8ef25a7882b0ebfd268e8a555ba9
swedge <scott.wedge@gmail.com>
2020-08-10 23:53:15 -0400

    Add 'exit 99' code

---
+++ sysinfo_return99.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script displays information about the system on which it is executed.
+
+
+# Tell the user that the script is starting.
+echo "Script starting ..."
+
+
+# Display the hostname of the system.
+echo "Running on host $HOSTNAME"
+
+
+# Display the current date and time when this information was collected.
+echo "Date and time is: `date`"
+
+# Display the kernel release followed by the architecture.
+echo "Kernel release is: `uname -r` and architecture is: `uname -m`"
+
+# Display the disk usage in a human readable format.
+echo "Disk usage in human readable format is: `df -h`"
+
+# End the script by letting the user know that it is done.
+echo "The script has completed."
+
+exit 99
