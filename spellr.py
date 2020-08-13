05d1ec226294c8a6c41f4e05ef9937b40551c950
swedge <scott.wedge@gmail.com>
2020-08-12 23:29:27 -0400

    first draft of scripts to find suspected spelling mistakes

---
+++ spellr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+read_file = open("t", "r")
+
+write_file = open("tt", "w")
+
+read_file.readlines()
+
+print(read_file)
---
+++ spellr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to grab suspect misspelled words
+# as last column in output of "spell -no"
+
+
+spell -no * >> t
+spell -no */* >> t
+
+
+
