9a5fac3597c31616c18de3071deaabe6b0b4aa74
swedge <scott.wedge@gmail.com>
2020-08-13 15:08:12 -0400

    Convert files to constants, add checks for made-up words

--- spellr.py
+++ spellr.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python3
 
-read_file = open("t", "r")
+""" Script that will evaluate each word.
+    If word seems valid, then write to write_file
 
-write_file = open("tt", "w")
+    Word is not valid if it contains "_" (ie 'first_field').
+    Word is not valid if it has a mix of upper case in middle of word (ie- madeUpWorddd)
 
-read_file.readlines()
+    Assumes list of words to be checked is in local file 'temp_in'.
+    Assumes output of valid words in in local file 'temp_out'.
+"""
+# declare variables and constants
+FILE_IN = "temp_in"
+FILE_OUT = "temp_out"
+FILE_OUT_INVALID = "temp_fail"
 
-print(read_file)
+# Open files to read from and to write to
+read_file = open(FILE_IN, "r")
+write_file = open(FILE_OUT, "a")
+fail_file = open(FILE_OUT_INVALID, "a")
+
+for j in read_file:
+    valid = True
+    
+    if j.lower() == j:
+        valid = True  # all lower case
+    elif "_" in j:
+        valid = False  # contains "_" so is made up word
+    elif j.lower().capitalize() == j: 
+        valid = True  # word is capitalized
+    el
+
+
+
+
+        write_file.write(j)
