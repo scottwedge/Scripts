a0ad144ccde67b19a9fe2fe49b83d829ed6933f4
swedge <scott.wedge@gmail.com>
2020-08-28 23:38:29 -0400

    Add echo statements announcing each layer of spell checking

--- many_dir_py.sh
+++ many_dir_py.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+echo "Starting first layer spell check"
 spell -no *.py > /tmp/t
+
+echo "Starting second layer spell check"
 spell -no */*.py >> /tmp/t
+
+echo "Starting third layer spell check"
 spell -no */*/*.py >> /tmp/t
+
+echo "Starting fourth layer spell check"
 spell -no */*/*/*.py >> /tmp/t
+
+echo "Starting fifth layer spell check"
 spell -no */*/*/*/*.py >> /tmp/t
+
+echo "Starting sixth layer spell check"
 spell -no */*/*/*/*/*.py >> /tmp/t
